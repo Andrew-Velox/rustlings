e3bab795a6c467c3f377d188f4c851010f4c9154
Andrew-Velox <mohabbat.bd2020@gmail.com>
2026-07-13 19:35:21 +0600

    solutions of quiz 2 and 12_opions exercsice

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,33 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String,Command)>) -> Vec<String> { 
+        
+        let mut res = Vec::new();
+
+        for (s,c) in input.iter(){
+
+            match c {
+                Command::Uppercase => res.push(s.to_uppercase()),
+                Command::Trim => res.push(s.trim().to_string()),
+                Command::Append(x) => {
+                    
+                    let mut get = s.to_string();
+                    for _ in 0..*x{
+                        get+="bar";
+
+                    }
+                    // println!("{get}");
+                    res.push(get);
+                }
+
+            }
+        }
+
+        res
+
+
+    }
 }
 
 fn main() {
@@ -37,7 +63,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
